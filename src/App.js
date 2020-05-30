20742947131d796e96571ab7993fcf259d401bdb
Patrick Ware <patrick.r.ware@gmail.com>
2020-05-29 23:14:29 -0700

    useEffect adjusted to have minMag and Maxmag within sam array [], old input and submit buttons removed

--- src/App.js
+++ src/App.js
@@ -35,44 +35,19 @@ function App() {
     });
   }
 
-  useEffect(doFetch,[minMag],[maxMag])
+  useEffect(doFetch,[minMag, maxMag])
 
   return (
   <div>
     <h1 className="title"> Shake Shack </h1>
     <h2 className="subtitle"> Significant Earthquakes in North America in 2020</h2>
     <div className="DataModifier">
-      <form className="FormClass" action="">
-        <label htmlFor="magnitude">Magnitude:</label>
-          <select className="Menu" id="magnitude" name="magnitude">
-          <option className="Menu-item" value="1.0">1.0</option>
-          <option className="Menu-item" value="1.5">1.5</option>
-          <option className="Menu-item" value="2.0">2.0</option>
-          <option className="Menu-item" value="2.5">2.5</option>
-          <option className="Menu-item" value="3.0">3.0</option>
-          <option className="Menu-item" value="3.5">3.5</option>
-          <option className="Menu-item" value="4.0">4.0</option>
-          <option className="Menu-item" value="4.5">4.5</option>
-          <option className="Menu-item" value="5.0">5.0</option>
-          <option className="Menu-item" value="5.5">5.5</option>
-          <option className="Menu-item" value="6.0">6.0</option>
-          <option className="Menu-item" value="6.5">6.5</option>
-          <option className="Menu-item" value="7.0">7.0</option>
-          <option className="Menu-item" value="7.5">7.5</option>
-          <option className="Menu-item" value="8.0">8.0</option>
-          <option className="Menu-item" value="8.5">8.5</option>
-          <option className="Menu-item" value="9.0">9.0</option>
-          <option className="Menu-item" value="9.5">9.5</option>
-          <option className="Menu-item" value="10.0">10.0</option>
-        </select>
-      </form>
       <form>
         <label htmlFor="minmag">Minimum Magnitude:</label>
           <input type="number" id="minmag" min="1" max="10" value={minMag} onChange={minimumMagnitude} />
         <label htmlFor="maxmag">Maximum Magnitude:</label>
           <input type="number" id="maxmag" min="1" max="10" value={maxMag} onChange={maximumMagnitude} />
       </form>
-      <button className="StdButton" onClick={doFetch}> Graph it! </button>
     </div>
     <div id="warning">
     </div>
