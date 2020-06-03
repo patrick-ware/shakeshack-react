074aab23059ed0558f8b52af7d5e49717e54d393
Patrick Ware <patrick.r.ware@gmail.com>
2020-06-02 22:04:55 -0700

    BarChart component created

---
+++ src/components/BarChart/BarChart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './BarChart.css';
+
+function BarChart(props) {
+  return (
+    <div className="BarChart">
+        {props.children}
+    </div>
+  );
+}
+
+export default BarChart;
