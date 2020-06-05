9197e842fc4af8fe22084c87dd87b070c534d1f7
Patrick Ware <patrick.r.ware@gmail.com>
2020-06-04 23:13:30 -0700

    warningmsg component now functional

--- src/components/WarningMsg/WarningMsg.js
+++ src/components/WarningMsg/WarningMsg.js
@@ -4,19 +4,19 @@ import './WarningMsg.css';
 function WarningMsg(props) {
   return (
     <div className="Warning">
-      {{props.dataLength} > 20 &&
+      {props.dataLength > 20 &&
         <p>
           Warning! Your selection has queried more records 
           than can be displayed on the graph ({props.dataLength} records). 
           Only the 20 most recent records will be shown.
         </p>
       }
-      {{props.dataLength} === 0 && minMag < maxMag &&
+      {props.dataLength === 0 && props.minMag < props.maxMag &&
         <p>
           Warning! Your selection has queried no records.
         </p>
       }
-      {minMag > maxMag &&
+      {props.minMag > props.maxMag &&
         <p>
           Warning! Minimum magnitude value must be less than maximum magnitude value.
         </p>
